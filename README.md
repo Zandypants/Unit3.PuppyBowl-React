612a4cc87676b61695278d17a812de25708d0253
Alexander Linse <zandervon24@gmail.com>
2024-04-01 10:14:14 -0500

    Init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Submission for Fullstack Academy Block 29 Workshop: Puppy Bowl React
+- Refactors a previous workshop into React.
+
+deployed app: TODO
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Puppies go here</h1>
+    </>
+  )
+}
+
+export default App
